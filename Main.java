35afa7542a95aa48b8c8592ea285c472da800410
seoe5580 <seoe5580@wrdsb.ca>
2020-10-08 04:04:03 +0000

    Done the factorial question

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 /**
- *
+ * This program calculates the factorial of user's number
  * @author 
  */
 public class Main {
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input a integer
+    System.out.println("Please enter an integer to calculate the factorial of");
+
+    // Gets the number from the user
+    int num = input.nextInt();
+
+    // declears a variable to 1 because if its 0 it will constantly mutiply it by 0
+    int multi = 1;
+
+    // using the loop it will calculate the factorial of user's integer
+    for(int i = 1; i <= num; i++){
+      multi = i * multi;
+    }
+    // tells the user the answer
+    System.out.println(num + "! = " + multi);
   }
 }
